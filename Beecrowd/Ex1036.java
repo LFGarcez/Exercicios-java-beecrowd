a19c8571bf853eddfc4b2067f44143cd65421f38
Luiz Felipe <123270303+LFGarcez@users.noreply.github.com>
2025-04-30 18:35:39 -0300

    Add files via upload

---
+++ Beecrowd/Ex1036.java
@@ -0,0 +1,31 @@
+import java .util.Scanner;
+import java.util.Locale;
+
+public class Ex1036 {
+    public static void main(String [] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+    
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double C = sc.nextDouble();
+        double delta = 0; 
+        double R1 = 0;
+        double R2 = 0;
+        
+        delta = Math.pow(B, 2) - 4 * (A * C);
+
+       
+        if(delta > 0 && A != 0){
+            R1 = (-B + Math.sqrt(delta)) / (2 * A);
+            R2 = (-B - Math.sqrt(delta)) / (2 * A); 
+
+            System.out.printf("R1 = %.5f%n", R1);
+            System.out.printf("R2 = %.5f%n", R2);
+        }else{
+            System.out.println("Impossivel calcular");
+        }
+
+        sc.close();
+    }
+}
